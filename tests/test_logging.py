16b310ceaa3268fa78450c6a74e80802ad550414
Phil Herbert <pherbert@thoughtworks.com>
2021-04-02 14:57:26 -0400

    adds service_id and service_name from app context to JSON logs

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,7 +1,26 @@
 import logging as builtin_logging
 import uuid
 
+import pytest
+from flask import g
+
 from notifications_utils import logging
+from notifications_utils.logging import AppContextFilter
+
+
+@pytest.fixture
+def log_record():
+    return builtin_logging.LogRecord(
+        name='some-logger',
+        level=0,
+        pathname='some-path',
+        lineno=95,
+        msg='some-msg',
+        args={},
+        exc_info=None,
+        func='some-func',
+        sinfo='some-info'
+    )
 
 
 def test_should_build_complete_log_line():
@@ -90,6 +109,26 @@ def test_should_build_statsd_line_without_service_id_or_time_taken():
         'status': 200
     }
     assert "method.endpoint.200" == logging.build_statsd_line(extra_fields)
+
+
+class TestAppContextFilter:
+
+    expected_fields_to_be_added_from_app_context = ['service_id', 'service_name']
+
+    @pytest.mark.parametrize('field_name', expected_fields_to_be_added_from_app_context)
+    def test_does_not_add_field_if_not_in_app_context(self, app, log_record, field_name):
+        with app.app_context():
+            AppContextFilter().filter(log_record)
+
+        assert not hasattr(log_record, field_name)
+
+    @pytest.mark.parametrize('field_name', expected_fields_to_be_added_from_app_context)
+    def test_adds_field_if_in_app_context(self, app, log_record, field_name):
+        with app.app_context():
+            setattr(g, field_name, 'some-value')
+            AppContextFilter().filter(log_record)
+
+        assert getattr(log_record, field_name) == 'some-value'
 
 
 def test_get_handlers_sets_up_logging_appropriately_with_debug(tmpdir):
